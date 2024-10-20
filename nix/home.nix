224613e4d914c5c5059de69b5d908532425f6025
Maulana Akmal <maulana.akmal@shopee.com>
2024-10-20 21:22:04 +0800

    Add tmux config

--- nix/home.nix
+++ nix/home.nix
@@ -12,6 +12,7 @@
   # need to add --impure args to darwin-rebuild
   home.file = {
     ".zshrc".source         = ~/dotfiles/zsh/.zshrc;
+    ".tmux.conf".source     = ~/dotfiles/tmux/.tmux.conf;
     ".config/kitty".source  = ~/dotfiles/kitty;
     ".config/nvim".source   = ~/dotfiles/nvim;
   };
