0aebd2a98b722295a32dbe3cdbcb1fb0d767e2e7
Maulana Akmal <maulana.akmal@shopee.com>
2024-11-24 19:50:41 +0800

    update some mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,17 +35,32 @@ vim.keymap.set('n', '<leader>f', find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>g', live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>b', telescope_builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>th', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>n', function() vim.cmd('Neotree float') end, { desc = 'Go to definition' })
 
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'LSP format' })
+vim.keymap.set('n', 'gi', telescope_builtin.lsp_implementations, { desc = 'Telescope buffers' })
+vim.keymap.set('n', 'gd', telescope_builtin.lsp_definitions, { desc = 'Telescope buffers' })
+vim.keymap.set('n', 'gc', telescope_builtin.lsp_incoming_calls, { desc = 'Telescope buffers' })
+
+-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'LSP format' })
+
+local function lsp_attached()
+    local clients = vim.lsp.get_active_clients({ bufnr = 0 }) -- Get active clients for the current buffer
+    return next(clients) ~= nil                               -- Returns true if there are any active clients
+end
+local function goToDefinition()
+    if lsp_attached() then
+        vim.lsp.buf.definition()
+    end
+
+
+end
+
+vim.keymap.set('n', '<C-N>', function() vim.cmd('Neotree toggle position=right') end, { desc = 'Go to definition' })
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { desc = 'LSP format' })
 vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = 'LSP rename' })
 vim.keymap.set('n', '<leader>lh', vim.lsp.buf.hover, { desc = 'LSP hover' })
 
-vim.keymap.set('n', '<leader>xx', function() vim.cmd('Trouble diagnostics toggle') end, {desc = 'diag'})
-
-vim.keymap.set('n', '<c-t>', '<CMD>exe v:count1 . "ToggleTerm direction=float"<CR>', { desc = 'toggle term' })
+vim.keymap.set('n', '<leader>xx', function() vim.cmd('Trouble diagnostics toggle win = { type = float } focus=true') end, { desc = 'diag' })
 
 
 -- vim.api.nvim_create_augroup("neotree", {})
