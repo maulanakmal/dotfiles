a4cbca3878ec52d272c82ca4c0acd830b01df4b2
Maulana Akmal <maulana2904@gmail.com>
2025-03-10 01:27:03 +0800

    Update

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -17,24 +17,49 @@
   };
 
   outputs = inputs@{
-    self, nix-darwin, nixpkgs,
+    self,
+    nix-darwin,
+    nixpkgs,
     nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle,
     home-manager
     }:
   let
-    common_configuration = {...}: {
+    configuration = { pkgs, ... }:
+    let
+      gvm = pkgs.stdenv.mkDerivation rec {
+        pname = "gvm";
+        version = "1.0";
+
+        src = pkgs.fetchFromGitHub {
+          # url = "https://github.com/moovweb/gvm.git";
+          owner = "moovweb";
+          repo = "gvm";
+          rev = "master";
+          sha256 = "sha256-dRFhvyOVUu1RjUpkjBneFWrCmnvSqO4Gyb6UR4bjboM=";
+        };
+
+        installPhase = ''
+          mkdir -p $out/share/gvm
+          cp -r ${src}/* $out/share/gvm
+        '';
+      };
+      in
+    {
       nix.settings.experimental-features = "nix-command flakes";
       services.nix-daemon.enable = true;
       system.configurationRevision = self.rev or self.dirtyRev or null;
       system.stateVersion = 5;
-
       programs.zsh.enable = true;  # default shell on catalina
-    };
 
-    
-    configuration = { pkgs, ... }: {
+      # system.defaults.finder.AppleShowAllFiles = true;
+      system.defaults.finder.ShowPathbar = true;
+      system.defaults.finder.ShowStatusBar = true;
+      system.defaults.finder.FXPreferredViewStyle = "Nlsv";
+      system.defaults.finder.FXDefaultSearchScope = "SCcf";
+      # system.defaults.finder.NewWindowTarget = "Home";
       environment.systemPackages =
         [
+          gvm
           # for dotfiles symlinking
           pkgs.stow
 
@@ -53,8 +78,8 @@
           pkgs.yq
 
           # for gvm (go version manager)
-          pkgs.bison
-          pkgs.mercurial
+          # pkgs.bison
+          # pkgs.mercurial
 
           # for python version management
           pkgs.pyenv
@@ -87,7 +112,6 @@
       homebrew = {
         enable = true;
         casks = [
-          "rectangle"
           "shottr"
           "kitty"
           "font-jetbrains-mono-nerd-font"
@@ -110,9 +134,8 @@
   in
   {
     darwinConfigurations = {
-      "shopee_macbook_pro" = nix-darwin.lib.darwinSystem {
+      "gojo" = nix-darwin.lib.darwinSystem {
         modules = [
-          common_configuration
           configuration
           {
             nixpkgs.hostPlatform = "x86_64-darwin";
@@ -127,13 +150,13 @@
             };
           }
 
-
           {
             users.users."maulana.akmal" = {
               name = "maulana.akmal";
               home = "/Users/maulana.akmal";
             };
           }
+
           home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
@@ -144,9 +167,9 @@
       };
 
       # macbook pro 2023 m2 pro
-      "mols_macbook_proc" = nix-darwin.lib.darwinSystem {
+      "toji" = nix-darwin.lib.darwinSystem {
+
         modules = [
-          common_configuration
           configuration
           {
             nixpkgs.hostPlatform = "aarch64-darwin";
@@ -156,10 +179,33 @@
           homebrew
           {
             nix-homebrew = {
-              user = "maulana.akmal";
+              user = "maulanakmal";
               enableRosetta = true;
             };
           }
+
+          {
+            users.users."maulanakmal" = {
+              name = "maulanakmal";
+              home = "/Users/maulanakmal";
+            };
+          }
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."maulanakmal" = {pkgs, ...}: {
+                # The state version is required and should stay at the version you
+                # originally installed.
+                home.stateVersion = "24.11";
+
+                home.username = "maulanakmal";
+                home.homeDirectory = "/Users/maulanakmal";
+                home.packages = [ pkgs.httpie ];
+              };
+          }
+
+
         ];
       };
     };
